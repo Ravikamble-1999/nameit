67dbf13a65f99fdea5324a4d726a029772205f24
Your Name <you@example.com>
2021-03-06 13:50:46 +0530

    Add the nameit project

---
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Namecard from "../Namecard/NameCard";
+import "./ResultsContainer.css";
+
+const ResultsContainer = ({ suggestedNames }) => {
+  const suggestedNameJsx = suggestedNames.map((suggestedName) => {
+    return <Namecard key={suggestedName} suggestedName={suggestedName} />;
+  });
+  return <div className="results-container">{suggestedNameJsx}</div>;
+};
+
+export default ResultsContainer;
